933e1ae939612cbbf9a2dc0b9cdb9c963f6a7fa6
Dharshini <dharshini.prasad@gmail.com>
2023-05-23 20:58:30 +0100

    add

--- ActionRecognition/clsHMDBModel.py
+++ ActionRecognition/clsHMDBModel.py
@@ -13,7 +13,7 @@ def train(model, data_loader, optimizer, device):
     label_ls = []
     pred_ls = []
     logits_ls = []
-
+    loss_ls = []
     tk = tqdm(data_loader, total=int(len(data_loader)), desc='Test', unit='frames', leave=False)
     for batch_idx, data in enumerate(tk):
         # fetch the data
@@ -36,11 +36,12 @@ def train(model, data_loader, optimizer, device):
         label_ls.append(label)
         pred_ls.append(pred)
         logits_ls.append(logits)
+        loss_ls.append(loss_this)
         # update the loss meter
         loss_meter.update(loss_this.item(), label.shape[0])
 
     print('Train: Average loss: {:.4f}\n'.format(loss_meter.avg))
-    return loss_meter.avg, label_ls, pred_ls, logits_ls
+    return loss_meter.avg, label_ls, pred_ls, logits_ls, loss_ls
 
 
 ##define test function
@@ -49,8 +50,11 @@ def test(model, data_loader, device):
     loss_meter = AverageMeter()
     acc_meter = AverageMeter()
     # switch to test mode
-    correct = 0
-    #model.eval()
+    label_test = []
+    pred_test = []
+    logits_test = []
+    loss_test = [] 
+    model.eval()
     tk = tqdm(data_loader, total=int(len(data_loader)), desc='Test', unit='frames', leave=False)
     for batch_idx, data in enumerate(tk):
         # fetch the data
@@ -66,28 +70,20 @@ def test(model, data_loader, device):
         logits_test = []
         with torch.no_grad():
             output = model(frame)
-        logits = output.logits
-        pred = logits.argmax(axis = 1).item()
+        logits = output
+        pred = logits.argmax(axis=1)
         
         # compute the loss function just for checking
-        loss_this = F.cross_entropy(logits, label)
-        # get the index of the max log-probability
-        # pred = output.argmax(dim=1, keepdim=True)
-        # check which of the predictions are correct
-        #correct_this = pred.eq(label.view_as(pred)).sum().item()
-        # accumulate the correct ones
-        #correct += correct_this
-        # compute accuracy
-        #acc_this = correct_this / label.shape[0] * 100.0
+        loss_this = F.cross_entropy(logits, label.long())     
         # update the loss and accuracy meter 
-        #acc_meter.update(acc_this, label.shape[0])
-        loss_meter.update(loss_this.item(), label.shape[0])
         label_test.append(label)
         pred_test.append(pred)
         logits_test.append(logits)
-        print('Test: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-        loss_meter.avg, correct, len(data_loader.dataset), acc_meter.avg))
-        return label_test, pred_test, logits_test
+        loss_test.append(loss_this)
+        loss_meter.update(loss_this.item(), label.shape[0])
+    return loss_meter.avg, label_test, pred_test, logits_test, loss_test
+    
+
 def topk_accuracy(y_true, y_pred, k=5):
     if y_pred.ndim == 1:
         y_pred = np.expand_dims(y_pred, axis=1)
